24ea0ba7a0c5eff072a53a330b0ef1947de2e537
akmal <islamov_akmal@inbox.ru>
2020-02-27 14:05:11 +0500

    фыва

--- js/script.js
+++ js/script.js
@@ -8,6 +8,24 @@ const S = (selector) => {
     return document.querySelectorAll(selector);
 }
 
+function deg2rad(deg) {
+    return deg * (Math.PI / 180)
+}
+
+function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
+    var R = 6371; // Radius of the earth in km
+    var dLat = deg2rad(lat2 - lat1);  // deg2rad below
+    var dLon = deg2rad(lon2 - lon1);
+    var a =
+        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+        Math.sin(dLon / 2) * Math.sin(dLon / 2)
+        ;
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var d = R * c; // Distance in km
+    return d;
+}
+
 const on = (event, elements, callback) => {
     if(typeof elements === 'string') {
         document.querySelectorAll(elements).forEach(one => {
@@ -251,3 +269,19 @@ ready(() => {
         });
     })
 });
+
+ready(() => {
+    navigator.geolocation.getCurrentPosition((res) => {
+        const coords = {
+            latitude: res.coords.latitude.toFixed(6),
+            longitude: res.coords.longitude.toFixed(6)
+        }
+
+        const distance = getDistanceFromLatLonInKm(
+            coords.latitude, coords.longitude, 41.33477, 69.370285
+        ).toFixed(3)
+
+        alert(`You're ${distance} meters away from the shop`)
+    })
+})
+
